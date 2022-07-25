eed708aade115742c8a7d94f2948a4e992d94dbf
Kiwicase <emailcaseydent@gmail.com>
2022-07-25 17:32:06 +0300

    List + creating random word generator from list

--- Hangman Game/Program.cs
+++ Hangman Game/Program.cs
@@ -1,26 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace HangmanGame
 {
     internal class Program
-{
-    static void Main(string[] args)
     {
-        string[] wordList = new string[10];
+        static void Main(string[] args)
+        {
+            string[] wordList = new string[10];
 
-        Console.WriteLine("The Hangman Game");
+            Console.WriteLine("The Hangman Game");
 
-        wordList[0] = "Trello";
-        wordList[1] = "Reverse";
-        wordList[2] = "Number";
-        wordList[3] = "Google";
-        wordList[4] = "Guessing";
-        wordList[5] = "Keyboard";
-        wordList[6] = "Github";
-        wordList[7] = "Data Types";
-        wordList[8] = "Floating Point";
-        wordList[9] = "Debugger";
-
+            wordList[0] = "Trello";
+            wordList[1] = "Reverse";
+            wordList[2] = "Number";
+            wordList[3] = "Google";
+            wordList[4] = "Guessing";
+            wordList[5] = "Keyboard";
+            wordList[6] = "Github";
+            wordList[7] = "Data Types";
+            wordList[8] = "Floating Point";
+            wordList[9] = "Debugger";
+            Random randWord = new Random();
+            int wordId = randWord.Next(0, 9);
+            string word = wordList[wordId];
+            char[] guess = new char[wordList.Length];
+            Console.WriteLine("Please enter your guess: ");
+        }
     }
-}
 }
