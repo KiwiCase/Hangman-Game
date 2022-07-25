5aa1f54afb677d8874049d85c2b7dcb54e7d0797
Kiwicase <emailcaseydent@gmail.com>
2022-07-25 14:53:04 +0300

    Add project files.

---
+++ Hangman Game/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Hangman Game
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] wordList = new string[10];
+
+        Console.WriteLine("The Hangman Game");
+
+        wordList[0] = "Trello";
+        wordList[1] = "Reverse";
+        wordList[2] = "Number";
+        wordList[3] = "Google";
+        wordList[4] = "Guessing";
+        wordList[5] = "Keyboard";
+        wordList[6] = "Github";
+        wordList[7] = "Data Types";
+        wordList[8] = "Floating Point";
+        wordList[9] = "Debugger";
+
+    }
+}
+}
